e45aee8a78c92d4853c1fd0a6d820dd5755b9b4c
da0shi <dev@da0shi.com>
2014-12-11 03:29:36 +0900

    Add script to insert CSS rule dynamically
    for function to switch
    * All messages
    * Only Messages which I post
    * Only Messages which Mention to me

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,43 @@
 (function ($, global, undefined) {
-  $(document).ready( function () {
   'use strict';
+  var dynamicStyle = {
+    style: null
+  };
+  dynamicStyle.initialize = function () {
+    var style = document.createElement('style');
+    style.setAttribute('media', 'screen');
+    // WebKit hack ...
+    style.appendChild(document.createTextNode(''));
+    document.head.appendChild(style);
+    dynamicStyle.style = style;
+  };
+  dynamicStyle.insert = function (selector, rules, index) {
+    if ('insertRule' in this.style.sheet) {
+      dynamicStyle.style.sheet.insertRule(selector +'{'+ rules +'}', index);
+    } else if ('addRule' in this.style.sheet) {
+      dynamicStyle.style.sheet.addRule(selector, rules, index);
+    }
+  }
+  dynamicStyle.remove = function (index) {
+    if (index === undefined) return false;
+    if ('deleteRule' in this.style.sheet) {
+      dynamicStyle.style.sheet.deleteRule(index);
+    } else if ('removeRule' in this.style.sheet) {
+      dynamicStyle.style.sheet.removeRule(index);
+    }
+    return true;
+  }
+  dynamicStyle.onlyMine = function () {
+    dynamicStyle.insert('div._message', 'display:none');
+  };
+  dynamicStyle.onlyMention = function () {
+  };
+  dynamicStyle.allMessages = function () {
+  }
+  $(document).ready( function () {
+    'use strict';
     chatwork.initialize();
+    dynamicStyle.initialize();
     console.log(chatwork);
   });
 })(jQuery, this);
