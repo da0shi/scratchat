0e9ae48b554859b8ac9c99ebcc13c204dba53cfe
da0shi <dev@da0shi.com>
2014-12-11 03:48:39 +0900

    insert, delete, and functions for show only mine/mention

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,13 @@
   'use strict';
   var dynamicStyle = {
     style: null
+    indexes: {
+      hideAll: -1,
+      hideNotMention: -1
+      hideNotMine: -1,
+    }
   };
+  var _index = dynamicStyle.indexes;
   dynamicStyle.initialize = function () {
     var style = document.createElement('style');
     style.setAttribute('media', 'screen');
@@ -11,12 +17,14 @@
     document.head.appendChild(style);
     dynamicStyle.style = style;
   };
-  dynamicStyle.insert = function (selector, rules, index) {
+  dynamicStyle.insert = function (selector, rules) {
+    var index = dynamicStyle.style.sheet.cssRules.length;
     if ('insertRule' in dynamicStyle.style.sheet) {
       dynamicStyle.style.sheet.insertRule(selector +'{'+ rules +'}', index);
     } else if ('addRule' in dynamicStyle.style.sheet) {
       dynamicStyle.style.sheet.addRule(selector, rules, index);
     }
+    return index;
   }
   dynamicStyle.remove = function (index) {
     if (index === undefined) return false;
@@ -28,11 +36,42 @@
     return true;
   }
   dynamicStyle.onlyMine = function () {
-    dynamicStyle.insert('div._message', 'display:none');
+    if (index.hideNotMention >= 0) {
+      dynamicStyle.remove(index.hideNotMention);
+      index.hideNotMention = -1;
+    }
+    if (index.hideNotMine < 0) {
+      index.hideNotMine = dynamicStyle.insert('div._message.chatTimeLineMessageMine', 'display: block;');
+    }
+    if (index.hideAll < 0) {
+      index.hideAll = dynamicStyle.insert('div._message', 'display:none');
+    }
   };
   dynamicStyle.onlyMention = function () {
+    if (index.hideNotMine >= 0) {
+      dynamicStyle.remove(index.hideNotMine);
+      index.hideNotMine = -1;
+    }
+    if (index.hideNotMention < 0) {
+      index.hideNotMention = dynamicStyle.insert('div._message.chatTimeLineMessageMention', 'display: block;');
+    }
+    if (index.hideAll < 0) {
+      index.hideAll = dynamicStyle.insert('div._message', 'display:none');
+    }
   };
   dynamicStyle.allMessages = function () {
+    if (index.hideAll >= 0) {
+      dynamicStyle.remove(index.hideAll);
+      index.hideAll = -1;
+    }
+    if (index.hideNotMention >= 0) {
+      dynamicStyle.remove(index.hideNotMention);
+      index.hideNotMention = -1;
+    }
+    if (index.hideNotMine >= 0) {
+      dynamicStyle.remove(index.hideNotMine);
+      index.hideNotMine = -1;
+    }
   }
   $(document).ready( function () {
     'use strict';
---
+++ scripts/scrachat.js
@@ -0,0 +1,5 @@
+(function (global) {
+  'use strict';
+  var scrachat = {};
+  global.scrachat = scrachat;
+})(this);
